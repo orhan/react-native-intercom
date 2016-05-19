f3d7fd3dadef866680a3dface8fdf9f09ac24111
Orhan Sönmez <orhnsnmz@googlemail.com>
2016-05-19 16:23:18 +0200

    Fixed typo in method name (which was missing an 's')

--- android/src/main/java/com/robinpowered/react/Intercom/IntercomModule.java
+++ android/src/main/java/com/robinpowered/react/Intercom/IntercomModule.java
@@ -110,7 +110,7 @@ public class IntercomModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void displayConversationList(Callback callback) {
+    public void displayConversationsList(Callback callback) {
         Intercom.client().displayConversationsList();
         callback.invoke(null, null);
     }
